16e34bd50e9fa63912ee6039537aa4ebb3979f0c
Bastien Graveleau <bastien.graveleau49@orange.fr>
2023-10-23 12:35:53 +0200

    git-add / getAll users function

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { getAll } = require("../controller/users.controller")
-router.post("/api/users", create);
+router.get("/", getAll);
 
 module.exports = router;
